2ec19b223c45f910e706fdacae9de04f1d6657b4
Erlan Torres <erlantorres@hotmail.com>
2024-07-22 09:50:56 +0200

    Initial code with the base code

---
+++ MissionMars/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using MissionMars;
+
+Rover rover = new Rover(0, 0, 'N');
+rover.Move("LMLMLMLMM");
+rover.PrintPosition();
+
+Console.ReadKey();
